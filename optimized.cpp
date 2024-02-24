326389dca2d324419d9acd971320f72587e0d8b2
Gregory Maldonado <gmaldona@oswego.edu>
2024-02-23 20:59:40 -0500

    memmap file

--- optimized.cpp
+++ optimized.cpp
@@ -7,6 +7,47 @@
 * date  : 2024-02-14
 */
 
-int main(int args, char** argv) {
-   return 0;
+#include <sys/mman.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "optimized.hpp"
+
+//======== GM ========================================================== 80 ====
+
+MappedFile::~MappedFile() {
+   delete this->map; 
 }
+
+MappedFile* map_file2mem(const char* path) {
+  auto mappedFile = new MappedFile();
+
+  // try to open the file and if fail return nullptr for main to handle
+  int fd; 
+  if ((fd = open(path, O_RDONLY)) == -1 ||
+      stat(path, &mappedFile->fileInfo) == -1) {
+    return nullptr;
+  }
+  mappedFile->map = (char*)mmap(0, mappedFile->fileInfo.st_size, PROT_READ,
+                                MAP_SHARED, fd, 0);
+  if (mappedFile->map == MAP_FAILED) {
+    return nullptr;
+  }
+  return mappedFile;
+}
+
+int main(int args, char** argv) {
+   
+   if (args == 1) {
+      printf("No file input.\n");
+      return 1;
+   }
+
+   auto* mapped_file = map_file2mem(argv[1]);
+
+}
+
+//======== GM ========================================================== 80 ====
--- optimized.hpp
+++ optimized.hpp
@@ -6,3 +6,28 @@
 * email : gmaldonado@cs.binghamton.edu
 * date  : 2024-02-14
 */
+
+#include <sys/stat.h>
+
+//======== GM ========================================================== 80 ====
+
+struct MappedFile {
+  struct stat fileInfo;
+  char *map;
+  ~MappedFile();
+};
+
+/**
+ * Borrowed from Gregory Maldonado's GitHub Repository:
+ * https://github.com/bu-cs447-2024-1s/one-billion-row-challenge-gmaldona 
+ * /blob/2008e90c7fe94a4d9515d09f94c597690e7a7e13/1brc.cpp#L65
+ * 
+ * Map a file to memory to faster reads.
+ *
+ * @param path: const char* path =>  string representation of a path to a file.
+ * 
+ * @returns: a pointer to the file contents.
+*/
+MappedFile* map_file2mem(const char* path);
+
+//======== GM ========================================================== 80 ====
